a675aa8545b8847a394235f4bcde27f05741af85
sahilbheke <sahilbheke3@gmail.com>
2024-07-14 01:19:37 +0530

    deposit and withdrawl function completed

---
+++ src/bank/management/system/Transaction.java
@@ -0,0 +1,110 @@
+ /*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bank.management.system;
+
+import java.awt.*;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.*;
+import java.awt.event.*;
+/**
+ *
+ * @author bheke
+ */
+public class Transaction extends JFrame implements ActionListener{
+    
+    JButton deposit, withdrawl, fastCash, miniStatement, pinChange, balanceEnquiry, exit;
+    String pinNumber;
+    
+    Transaction(String pinNumber){
+        
+        this.pinNumber = pinNumber;
+        
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0, 900, 900);
+        add(image);
+        
+        JLabel text = new JLabel("Please select your Transaction");
+        text.setBounds(215, 300, 700, 35);
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("System", Font.BOLD, 16));
+        image.add(text);
+        
+        deposit = new JButton("Deposit");
+        deposit.setBounds(170, 415, 150, 30);
+        deposit.addActionListener(this);
+        image.add(deposit);
+        
+        withdrawl = new JButton("Cash Withdrawl");
+        withdrawl.setBounds(355, 415, 150, 30);
+        withdrawl.addActionListener(this);
+        image.add(withdrawl);
+        
+        fastCash = new JButton("Fast Cash");
+        fastCash.setBounds(170, 450, 150, 30);
+        fastCash.addActionListener(this);
+        image.add(fastCash);
+        
+        miniStatement = new JButton("Mini Statement");
+        miniStatement.setBounds(355, 450, 150, 30);
+        miniStatement.addActionListener(this);
+        image.add(miniStatement);
+        
+        pinChange = new JButton("Pin Change");
+        pinChange.setBounds(170, 485, 150, 30);
+        pinChange.addActionListener(this);
+        image.add(pinChange);
+        
+        balanceEnquiry = new JButton("Balance Enquiry");
+        balanceEnquiry.setBounds(355, 485, 150, 30);
+        balanceEnquiry.addActionListener(this);
+        image.add(balanceEnquiry);
+        
+        exit = new JButton("Exit");
+        exit.setBounds(355, 520, 150, 30);
+        image.add(exit);
+        exit.addActionListener(this);
+        
+        setSize(900, 900);
+        setLocation(300, 0);
+        setUndecorated(true);
+        setVisible(true);
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        if(ae.getSource() == exit){
+            System.exit(0);
+        }
+        else if(ae.getSource() == deposit){
+            setVisible(false);
+            new Deposit(pinNumber).setVisible(true);
+        }
+        else if(ae.getSource() == withdrawl){
+            setVisible(false);
+            new Withdrawl(pinNumber).setVisible(true);
+        }
+        else if(ae.getSource() == fastCash){
+            
+        }
+        else if(ae.getSource() == miniStatement){
+            
+        }
+        else if(ae.getSource() == pinChange){
+            
+        }
+        else if(ae.getSource() == balanceEnquiry){
+            
+        }
+    }
+    
+    public static void main(String args[]){
+        new Transaction("");
+    }
+}
---
+++ src/bank/management/system/Withdrawl.java
@@ -0,0 +1,101 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package bank.management.system;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+
+/**
+ *
+ * @author bheke
+ */
+public class Withdrawl extends JFrame implements ActionListener{
+    
+    JTextField amountTextFeild;
+    JButton withdraw, back;
+    String pinNumber;
+            
+    Withdrawl(String pinNumber){
+        
+        this.pinNumber = pinNumber;
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/atm.jpg"));
+        Image i2 = i1.getImage().getScaledInstance(900, 900, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(0, 0 , 900, 900);
+        add(image);
+        
+        // Deposit
+        
+        JLabel text = new JLabel("Enter the amount you want to Withdraw");
+        text.setForeground(Color.WHITE);
+        text.setFont(new Font("System", Font.BOLD, 16));
+        text.setBounds(170, 300, 400, 20);
+        image.add(text);
+        
+        amountTextFeild = new JTextField();
+        amountTextFeild.setFont(new Font("Raleway", Font.BOLD, 22));
+        amountTextFeild.setBounds(175, 350, 320, 25);
+        image.add(amountTextFeild);
+        
+        withdraw = new JButton("Withdraw");
+        withdraw.setBounds(260, 390, 140, 25);
+        withdraw.addActionListener(this);
+        image.add(withdraw);
+        
+        back = new JButton("Back");
+        back.setBounds(355, 520, 140, 25);
+        back.addActionListener(this);
+        image.add(back);
+        
+        setLayout(null);
+        setSize(900, 900);
+        setLocation(300, 0);
+        setVisible(true);
+
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        
+        String amount = amountTextFeild.getText();
+   
+        
+        if(ae.getSource() == withdraw){
+            long numericAmount = 1;
+            Date date = new Date();
+            
+            try{
+                numericAmount = Long.parseLong(amount); 
+            }
+            catch(Exception e){
+                JOptionPane.showMessageDialog(null, "Enter valid amount between Rs1 to Rs100000");
+            }
+            
+            if(numericAmount < 1 || numericAmount > 100000){
+                JOptionPane.showMessageDialog(null, "Enter valid amount between Rs1 to Rs100000");
+            }else{
+                try{
+                    Conn c = new Conn();
+                    String query = "insert into bank values('"+pinNumber+"', '"+date+"', '"+"Withdraw"+"', '"+amount+"')";
+                    c.s.executeUpdate(query); 
+                    JOptionPane.showMessageDialog(null, "Rs " + amount + " Withdraw Successfully");
+                }
+                catch(Exception e){
+                    System.out.print(e);
+                }  
+            }
+        }
+        else if(ae.getSource() == back){
+            setVisible(false);
+            new Transaction(pinNumber).setVisible(true);
+        }
+    }
+    public static void main(String args[]){
+        new Withdrawl("");
+    }    
+}
